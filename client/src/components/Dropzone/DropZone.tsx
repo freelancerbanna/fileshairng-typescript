bd4229c57025aa023882e8e32ec6478a2041d4f1
freelancerbanna <hasanulhaquebanna@gmail.com>
2022-06-08 04:49:40 +0600

    conditionally file supproting added

--- client/src/components/Dropzone/DropZone.tsx
+++ client/src/components/Dropzone/DropZone.tsx
@@ -1,3 +1,4 @@
+import classNames from "classnames";
 import Image from "next/image";
 import React, { useCallback } from "react";
 import { useDropzone } from "react-dropzone";
@@ -6,15 +7,28 @@ const DropZone = () => {
   const onDrop = useCallback((acceptedFiles: any) => {
     console.log(acceptedFiles);
   }, []);
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+  const {
+    getRootProps,
+    getInputProps,
+    isDragActive,
+    isDragAccept,
+    isDragReject,
+  } = useDropzone({
     onDrop,
     multiple: false,
+    accept: { "image/*": ["png,jpeg,jpg,gif"], "audio/*": ["mpeg,ogg,mp3"] },
   });
   return (
     <div className="p-4 w-[95%]">
       <div {...getRootProps()}>
         <input {...getInputProps()} />
-        <div className="flex flex-col items-center justify-center border-2 border-dashed w-full p-5 border-[#9de1d7] rounded-[5px]">
+        <div
+          className={classNames(
+            "flex flex-col items-center justify-center border-2 border-dashed w-full p-5 border-[#9de1d7] rounded-[5px]",
+            isDragReject && "border-red-600",
+            isDragAccept && "border-green-600"
+          )}
+        >
           <div className="w-32 h-16 overflow-hidden relative">
             <Image
               src="/image/fileshare.png"
@@ -22,8 +36,16 @@ const DropZone = () => {
               objectFit="contain"
             />
           </div>
-          <p>Just Drag and Drop!</p>
-          <div className="mt-4">Only jpeg,jpg,png and mp3 file supported</div>
+          {isDragReject ? (
+            <p className="text-red-600">This type of file is not supported</p>
+          ) : (
+            <>
+              <p>Just Drag and Drop!</p>
+              <div className="mt-4">
+                Only jpeg,jpg,png and mp3 file supported
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
