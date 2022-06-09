094e18bb632149a9f776687a171186f8a9b1609b
freelancerbanna <hasanulhaquebanna@gmail.com>
2022-06-09 15:37:02 +0600

    react spinners package added

--- client/src/components/Dropzone/index.tsx
+++ client/src/components/Dropzone/index.tsx
@@ -1,12 +1,14 @@
 import axios from "axios";
-import React, { FormEventHandler, useState } from "react";
+import React, { useState } from "react";
 import Swal from "sweetalert2";
+import ScaleLoader from "react-spinners/ScaleLoader";
 
 //
 import DropZone from "./DropZone";
 import RenderZone from "./RenderZone";
 
 const DropzoneWrapper = () => {
+  const [loading, setLoading] = useState(false);
   const [file, setFile] = useState("");
   const [id, setId] = useState(null!);
   const [donwloadLink, setDownloadLink] = useState(null!);
@@ -15,35 +17,50 @@ const DropzoneWrapper = () => {
     e.preventDefault();
     const formData = new FormData();
     formData.append("filesharing", file);
-
-    try {
-      const { data } = await axios({
-        method: "POST",
-        data: formData,
-        url: "/upload/file",
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      });
-      data &&
-        Swal.fire({
-          title: "Successfully uploaded",
-          text: "You are almosst therer! Please see a donwload link will be there copy and paste that to new tab from your browser",
-          icon: "success",
-          confirmButtonText: "Ok",
+    setLoading(true);
+    //
+    setTimeout(async () => {
+      try {
+        const { data } = await axios({
+          method: "POST",
+          data: formData,
+          url: "/upload/file",
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
         });
+        data && setLoading(false);
+        data &&
+          Swal.fire({
+            title: "Successfully uploaded",
+            text: "You are almosst therer! Please see a donwload link will be there copy and paste that to new tab from your browser",
+            icon: "success",
+            confirmButtonText: "ok",
+          });
 
-      data && setFile("");
-      data && setDownloadLink(data.downloadLink);
-      data && setId(data.id);
-    } catch (error) {
-      console.log(error);
-    }
+        data && setFile("");
+        data && setDownloadLink(data.downloadLink);
+        data && setId(data.id);
+      } catch (error: any) {
+        Swal.fire({
+          title: "Error!",
+          text: error.response.data,
+          icon: "error",
+          confirmButtonText: "ok",
+        });
+      }
+    }, 1000);
   };
   return (
     <div className="w-[500px] h-auto flex items-center justify-center bg-[#15293a66] rounded-[10px] border-4 border-[#131332] shadow-2xl  flex-col">
-      <DropZone setFile={setFile} />
-      {file && <RenderZone file={file} handleSubmit={handleSubmit} />}
+      {loading ? (
+        <ScaleLoader />
+      ) : (
+        <>
+          <DropZone setFile={setFile} />
+          {file && <RenderZone file={file} handleSubmit={handleSubmit} />}
+        </>
+      )}
     </div>
   );
 };
