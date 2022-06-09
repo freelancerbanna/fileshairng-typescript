a6d7ccf2765573d7f44730606c2ca7a1836fb8f3
freelancerbanna <hasanulhaquebanna@gmail.com>
2022-06-09 14:39:34 +0600

    upload function is working fine

--- client/src/components/Dropzone/index.tsx
+++ client/src/components/Dropzone/index.tsx
@@ -1,14 +1,33 @@
+import axios from "axios";
 import React, { FormEventHandler, useState } from "react";
 import DropZone from "./DropZone";
 import RenderZone from "./RenderZone";
 
 const DropzoneWrapper = () => {
   const [file, setFile] = useState(null!);
-  const handleSubmit = (e: any) => {
+  const [id, setId] = useState(null!);
+  const [donwloadLink, setDownloadLink] = useState(null!);
+  //
+  const handleSubmit = async (e: any) => {
     e.preventDefault();
     const formData = new FormData();
-    // formData.append("filesharing", file);
-    alert("Hell");
+    formData.append("filesharing", file);
+
+    try {
+      const { data } = await axios({
+        method: "POST",
+        data: formData,
+        url: "/upload/file",
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      setDownloadLink(data.downloadLink);
+      setId(data.id);
+    } catch (error) {
+      console.log(error);
+    }
   };
   return (
     <div className="w-[500px] h-auto flex items-center justify-center bg-[#15293a66] rounded-[10px] border-4 border-[#131332] shadow-2xl  flex-col">
--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,7 +1,10 @@
 import "styles/globals.css";
 import type { AppProps } from "next/app";
+import axios from "axios";
 
 import { Header } from "components";
+
+axios.defaults.baseURL = "http://localhost:8000/api";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
