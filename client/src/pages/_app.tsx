ab6f70a300cce66273665150ea646bfdb56b04b8
freelancerbanna <hasanulhaquebanna@gmail.com>
2022-06-11 06:08:48 +0600

    configured

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -5,7 +5,7 @@ import axios from "axios";
 
 import { Header } from "components";
 
-axios.defaults.baseURL = "http://localhost:8000/api";
+// axios.defaults.baseURL = "http://localhost:8000/api";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
